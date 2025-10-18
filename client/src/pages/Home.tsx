5c611f453ca25bee0d17683bbe4378c52cc13f0a
princesison <prince.sison@nlplogix.com>
2025-10-19 02:58:17 +0800

    import bootstrap used for UI library and fix on navbar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import { Navbar } from "../components/shared/Navbar/Navbar";
-import "./Home.scss";
 
 export const Home = () => {
   return (
-    <div className="home-page">
+    <div className="container mx-auto p-4">
       <Navbar />
     </div>
   );
