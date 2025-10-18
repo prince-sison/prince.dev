1efd327bf950b9082e5df295e7b2c9b9c8520200
princesison <prince.sison@nlplogix.com>
2025-10-19 03:12:34 +0800

    Add animation on the logo

--- client/src/components/shared/Navbar/Navbar.tsx
+++ client/src/components/shared/Navbar/Navbar.tsx
@@ -2,9 +2,26 @@ import { Nav } from "react-bootstrap";
 import "./Navbar.scss";
 
 export const Navbar = () => {
+  const handleLogoClick = () => {
+    // Navigate to home - you can use different methods:
+    // Option 1: Scroll to top of page
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    // Option 2: Navigate to home URL (uncomment if needed)
+    // window.location.href = '/';
+
+    // Option 3: If using React Router, you would use navigate('/') here
+  };
+
   return (
     <div className="navbar-container">
-      <div className="logo">Prince.Dev</div>
+      <div
+        className="d-flex align-items-center justify-content-center p-2 logo"
+        onClick={handleLogoClick}
+      >
+        <img src="/code-icon.svg" alt="logo" />
+        Prince.Dev
+      </div>
       <Nav className="nav-links">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#about">About</Nav.Link>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../components/shared/Navbar/Navbar";
 
 export const Home = () => {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container">
       <Navbar />
     </div>
   );
