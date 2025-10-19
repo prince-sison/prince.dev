5f3144d3bf339a688a257c30680751dc027c3f1e
princesison <prince.sison@nlplogix.com>
2025-10-19 20:06:20 +0800

    code for hero section

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -3,8 +3,17 @@ import "./Hero.scss";
 export const Hero = () => {
   return (
     <div className="hero-container">
-      <span className="hero-text">Welcome to Prince.Dev</span>
-      <img src="/hero-landing.jpg" alt="Hero Image" className="hero-image" />
+      <div className="hero-summary">
+        <h2>Hi,</h2>
+        <h1>I'm Prince</h1>
+        <h3>Full Stack Developer</h3>
+        <p>
+          Welcome to my portfolio website. Here you can find my projects and
+          skills.
+        </p>
+        Hi, I'm Prince — a Software Engineer Who Loves Building Web Apps.
+      </div>
+      <img src="/developer_male.jpg" alt="Hero Image" className="hero-image" />
     </div>
   );
 };
